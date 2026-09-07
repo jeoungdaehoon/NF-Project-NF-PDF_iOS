b25f9536354f0184d064580dc4b0c495074eb57d
JoungDaeHoon <>
2026-09-07 17:08:39 +0900

    Update macOS app icon and bump build to 40

--- NFMac/MacPortalModels.swift
+++ NFMac/MacPortalModels.swift
@@ -27,7 +27,7 @@ enum MacAppVersion {
     }
 
     static var build: String {
-        Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? "39"
+        Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? "40"
     }
 
     static var displayText: String { "v\(number) Build \(build)" }
